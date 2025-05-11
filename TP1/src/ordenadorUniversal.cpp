8ea48a6c044e87f9dca9bcb64cb10ff76f6d744f
Lara-Amelia <laraameliamf@gmail.com>
2025-05-11 00:21:14 -0300

    atualização no arquivo cpp da classe OrdenadorUniversal, com foco na forma de utilização da struct contador e do array stats, além da adição dos métodos para geração e atualização de estatisticas

--- TP1/src/ordenadorUniversal.cpp
+++ TP1/src/ordenadorUniversal.cpp
@@ -14,37 +14,54 @@ ordUniversal::ordUniversal(int A, int B, int C, int limCusto)
     nroQuebras = 0;
 }
 
-void ordUniversal::ordenadorUniversal(int* V, int tam, int MinTamParticao, int limiarQuebras)
+int ordUniversal:: calculaQuebras(int* vetor, int tam)
 {
-    contador_t s;
+    int nroQuebras = 0;
+    for(int i = 1; i < tam; i++)
+    {
+        if(vetor[i] < vetor[i-1]) //se o atual for menor que o seu antecessor
+            nroQuebras++;
+    }
+    return nroQuebras;
+}
+
+void ordUniversal::ordenadorUniversal(int* V, int tam, int MinTamParticao, int limiarQuebras, contador_t* s)
+{
     if(nroQuebras < limiarQuebras)
     {
-        insercao (V, tam, &s);
+        insercao (V, tam, s);
     } 
     else 
     {
         if(tam > minTamParticao)
         {
-            quickSort(V, 0, (tam-1), &s) ;
+            quickSort(V, 0, (tam-1), s) ;
         } 
         else
         {
-            insercao (V, tam, &s) ;
+            insercao (V, tam, s) ;
         }
     }
 }
 
 //fará o cálculo do custo com base nos stats obtidos
-void ordUniversal::registraEstatisticas(double* custo)
+void ordUniversal::registraEstatisticas(double* custo, contador_t* stats)
 {
-
+    *(custo) = (double)(coefA*stats->cmp) + (coefB*stats->move) + (coefC*stats->calls);
 }
 
-void ordUniversal::imprimeEstatisticas(double*custo)
+void ordUniversal::imprimeEstatisticas(double* custo, contador_t* stats, int t, int numMPS, double diffCusto)
 {
-
+    std::cout << "iter " << numMPS << " " << std::endl;
+    std::cout << "mps " << t << "cost " << *(custo) << " cmp " << stats->cmp 
+              << " move " << stats->move << " calls " << stats->calls << " " << std::endl;
+    if(numMPS == 5)
+    {
+        std::cout << "nummps 6 limParticao " << t << " mpsdiff " << diffCusto << std::endl;
+    }         
 }
 
+//ACHO que está pronto
 int ordUniversal::determinaLimiarParticao(int* v, int tam, int limiarCusto)
 {
     int minMPS = 2; //menor partição possível
@@ -53,16 +70,17 @@ int ordUniversal::determinaLimiarParticao(int* v, int tam, int limiarCusto)
     int diffCusto = limiarCusto + 1; //iniciamos com um custo maior que o limiar - T na 1° iter
     double custo[6];
     contador_t stats[6];
-    int limParticao;
-    //mps na impressão será o valor de T
+    int limParticao = 2;
+    int numMPS = 6; //tamanho máximo de numMPS
+    //mps na impressão será o valor de t, nro da iteração será numMPS
     while((diffCusto > limiarCusto) && (numMPS >= 5))
     {
         numMPS=0;
         for(int t=minMPS; t<=maxMPS; t+=passoMPS) //para cada tamanho possível de partição
         {
-            ordenadorUniversal(v, tam , t , tam);
-            registraEstatisticas(custo[numMPS]); //passa um ponteiro para a posição no array custo
-            imprimeEstatisticas(custo[numMPS]); //modificaremos o seu valor
+            ordenadorUniversal(v, tam , t , tam, &stats[numMPS]);
+            registraEstatisticas(&custo[numMPS], &stats[numMPS]); //passa um ponteiro para a posição no array custo
+            imprimeEstatisticas(&custo[numMPS], stats, limParticao, numMPS, diffCusto); //modificaremos o seu valor
             numMPS++;
         }
         limParticao = menorCusto();
