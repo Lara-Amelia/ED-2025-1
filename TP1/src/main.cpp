adcd432c63adcb06cedb4ecd7678aaa2ebf9be0b
Lara-Amelia <laraameliamf@gmail.com>
2025-05-20 18:49:43 -0300

    adição de tratamento de exceções para leitura de dados do arquivo de entrada na main. FALTA APAGAR OS COMENTÁRIOS DE DEPURAÇÃO

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -6,77 +6,116 @@
 #include <exception>
 #include <fstream>
 
-int main(int agrc, char** argv)
+int main(int argc, char** argv)
 {
-    std::string nomeArquivo = argv[1];
-    //adicionar exceção para caso argv < 1
-    std::ifstream arquivo(nomeArquivo);
-    //adicionar exceção caso o arquivo não seja aberto
-    std::string linha; 
-    int lCusto, qtChaves, seedArquivo;
-    double A, B, C;
-    //melhorar leitura
-     if (std::getline(arquivo, linha)) {
-        std::istringstream iss(linha);
-        iss >> seedArquivo;
-    }
-
-
-    if (std::getline(arquivo, linha)) {
-        std::istringstream iss(linha);
-        iss >> lCusto;
-    }
-
-    if (std::getline(arquivo, linha)) {
-        std::istringstream iss(linha);
-        iss >> A;
-    }
-
-    if (std::getline(arquivo, linha)) {
-        std::istringstream iss(linha);
-        iss >> B;
-    }
-
-    if (std::getline(arquivo, linha)) {
-        std::istringstream iss(linha);
-        iss >> C;
-    }
-
-    if (std::getline(arquivo, linha)) {
-        std::istringstream iss(linha);
-        iss >> qtChaves;
-    } 
-
-
-    /*arquivo >> lCusto >> A >> B >> C >> qtChaves;*/
-
-    ordUniversal ordUN(A, B, C, lCusto, seedArquivo);
-    /*std::cout << "seed: " << seedArquivo << std::endl;
-    std::cout << "lCusto: " << lCusto << "\n";
-    std::cout << "A: " << A << "\n";
-    std::cout << "B: " << B << "\n";
-    std::cout << "C: " << C << "\n";
-    std::cout << "qtChaves: " << qtChaves << "\n";*/
-
-    int vetor[qtChaves];
-    for(int i = 0; i < qtChaves; i++)
+    try 
     {
-         if (std::getline(arquivo, linha))
+        if(argc < 2)
+        {
+            throw std::invalid_argument("ERRO: o nome do arquivo não foi fornecido na linha de comando");
+        }
+
+        std::string nomeArquivo = argv[1];
+        std::ifstream arquivo(nomeArquivo);
+
+        if(!arquivo.is_open())
+        {
+            throw std::runtime_error("ERRO: não foi possível abrir o arquivo " + nomeArquivo);
+        }
+        
+        std::string linha; 
+        int lCusto, qtChaves, seedArquivo;
+        double A, B, C;
+        
+
+        //Faz a leitura das linhas com dados sobre o ordenador do arquivo 
+        if (std::getline(arquivo, linha)) 
         {
             std::istringstream iss(linha);
-            int value;
-            if(iss >> value)
-            {
-                //fazer checagens extras de tamanho
-                vetor[i] = value;
-                //std::cout << "valor na posição i do vetor: " << vetor[i] << std::endl;
-            }
+            iss >> seedArquivo;
         }
+        else
+            throw std::runtime_error("ERRO: não foi possível ler o valor de seed");
+
+        if (std::getline(arquivo, linha)) 
+        {
+            std::istringstream iss(linha);
+            iss >> lCusto;
+        }
+        else
+            throw std::runtime_error("ERRO: não foi possível ler o valor de Limiar de Custo");
+
+        if (std::getline(arquivo, linha)) 
+        {
+            std::istringstream iss(linha);
+            iss >> A;
+        }
+        else
+            throw std::runtime_error("ERRO: não foi possível ler o valor do coeficiente a");
+
+        if (std::getline(arquivo, linha)) 
+        {
+            std::istringstream iss(linha);
+            iss >> B;
+        }
+        else
+            throw std::runtime_error("ERRO: não foi possível ler o valor do coeficiente b");
+
+        if (std::getline(arquivo, linha)) 
+        {
+            std::istringstream iss(linha);
+            iss >> C;
+        }
+        else
+            throw std::runtime_error("ERRO: não foi possível ler o valor do coeficiente c");
+
+        if (std::getline(arquivo, linha)) 
+        {
+            std::istringstream iss(linha);
+            iss >> qtChaves;
+        }
+        else
+            throw std::runtime_error("ERRO: não foi possível ler o valor do tamanho do vetor");
+
+        /*arquivo >> lCusto >> A >> B >> C >> qtChaves;*/
+
+        ordUniversal ordUN(A, B, C, lCusto, seedArquivo);
+        /*std::cout << "seed: " << seedArquivo << std::endl;
+        std::cout << "lCusto: " << lCusto << "\n";
+        std::cout << "A: " << A << "\n";
+        std::cout << "B: " << B << "\n";
+        std::cout << "C: " << C << "\n";
+        std::cout << "qtChaves: " << qtChaves << "\n";*/
+
+        //Faz a leitura das qtChaves linhas do arquivo contendo os elementos do vetor
+        int vetor[qtChaves];
+        for(int i = 0; i < qtChaves; i++)
+        {
+            if (std::getline(arquivo, linha))
+            {
+                std::istringstream iss(linha);
+                int value;
+                if(iss >> value)
+                {
+                    //fazer checagens extras de tamanho
+                    vetor[i] = value;
+                    //std::cout << "valor na posição i do vetor: " << vetor[i] << std::endl;
+                }
+            }
+            else
+                throw std::runtime_error("ERRO: fim da leitura, não há linhas suficientes no arquivo de entrada");
+        }
+
+        std::cout << "size " << qtChaves << " seed " << seedArquivo << " breaks " << ordUN.calculaQuebras(vetor, qtChaves);
+        std::cout << std::endl << std::endl;
+        arquivo.close();
+        int limtamParticao = ordUN.determinaLimiarParticao(vetor, qtChaves, lCusto);
+        ordUN.determinaLimiarQuebras(vetor, qtChaves, lCusto, limtamParticao);
+    }    
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 1;
     }
-    std::cout << "size " << qtChaves << " seed " << seedArquivo << " breaks " << ordUN.calculaQuebras(vetor, qtChaves);
-    std::cout << std::endl << std::endl;
-    arquivo.close();
-    int limtamParticao = ordUN.determinaLimiarParticao(vetor, qtChaves, lCusto);
-    ordUN.determinaLimiarQuebras(vetor, qtChaves, lCusto, limtamParticao);
     return 0;
 }
