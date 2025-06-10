369d6df275c9897d52d7d0c32d0c9c98ec2c085a
Lara-Amelia <laraameliamf@gmail.com>
2025-06-10 20:42:13 -0300

    alterações na interface e implementação dos métodos do TAD Armazem, com melhorias quanto ao uso de Seções

--- TP2/include/armazem.hpp
+++ TP2/include/armazem.hpp
@@ -15,7 +15,9 @@ class Secao
 
     public:
     Secao();
+    Secao(int dest);
     void Limpa();
+    void setDestino(int n);
 
     friend class Armazem;
 };
@@ -26,26 +28,36 @@ class Armazem
     private:
     Secao* secoes; //será usado como um vetor de seções com tamanho igual à quantidade de adjacencias do vértice
     int nroAdj; //registra o número de adjacências do vértice/armazém - tamanho do vetor de seções
+    int* vizinhos; //vetor para registrar os vizinhos do grafo - a partir dele chamaremos os construtores para seções
     int idGrafo; //armazena a id do armazem no grafo - pode facilitar pesquisas no grafo
+    int custoRemocao;
+
+    public:
+    Armazem(int nAdj, int idG, int custo);
+    ~Armazem();
 
     
-    public:
-    void armazenaPacote(Pacote item, int destino); //insere o pacote na pilha principal da seção correspondente
+    void armazenaPacote(Pacote item, Secao secao); //insere o pacote na pilha principal da seção correspondente
+
+    //método que definirá com qual seção estamos lidando
+    //deve ser chamado antes do início de operações que utilizaram uma certa seção,
+    //para que não precisemos rebuscar a seção sempre
+    int encontraSecao(int destino);
 
     //deve contabilizar o tempo necessário para recuperar o pacote de fato
     //o destino pode ser obtido a partir da rota
-    Pacote recuperaPacote(int identificador, int destino);
+    //Pacote recuperaPacote(int identificador, int destino);
 
     //transfere os pacotes da pilha principal para a auxiliar 
-    void esvaziaPrincipal(Pilha principal, Pilha auxiliar, int destino); 
+    void esvaziaPrincipal(Secao secao); 
 
     //retira os capacidade primeiros elementos da pilha auxiliar
-    void carregaTransporte(Pilha auxiliar, int capacidade, int destino);
+    void carregaTransporte(int capacidade, int destino, Secao secao);
 
     //retorna os elementos que ficaram na pilha auxiliar para a principal - pacote rearmazenado
     //talvez o parâmetro possa ser só a seção (em todos os lugares onde passamos ambas as pilhas)
     //é melhor do que sempre buscar a seção de referência
-    void retornaPrincipal(Pilha principal, Pilha auxiliar);
+    void retornaPrincipal(Secao secao);
 };
 
 #endif
---
+++ TP2/src/armazem.cpp
@@ -0,0 +1,89 @@
+#include "armazem.hpp"
+#include <iostream>
+
+Secao::Secao()
+{
+    //o cosntrutor default das pilhas é chamado automaticamente
+    destino = -1; //flag para destino ainda não definido
+}
+
+Secao::Secao(int dest)
+{
+    destino = dest;
+}
+
+Armazem::Armazem(int nAdj, int idG, int custo)
+{
+    nroAdj = nAdj;
+    idGrafo = idG;
+    secoes = new Secao[nAdj];
+    vizinhos = new int[nAdj];
+    custoRemocao = custo;
+}
+
+Armazem::~Armazem()
+{
+    delete[] secoes;
+    delete[] vizinhos;
+}
+
+int Armazem::encontraSecao(int destino)
+{
+    for(int i = 0; i < nroAdj; i++)
+    {
+        if(this->secoes[i].destino == destino)
+            return i;
+    }
+    //throw "ERRO: secao não encontrada no armazém"
+}
+
+//a seção passada será acessada no vetor secoes utilizando o índice fornecido pelo método encontraSecao
+//para qualquer operação similar, essa será a abordagem empregada
+void Armazem::armazenaPacote(Pacote item, Secao secao)
+{
+    //insere o pacote na pilha principal da seção correspondente
+    secao.principal.Empilha(item);
+    //antes deste tipo de mensagem, também temos de imprimir o tempo em que ocorreu
+    //provavelmente faremos isso no próprio escalonador
+    std::cout << "pacote " << item.getId() << " armazenado em " << idGrafo << " na secao " << secao.destino << std::endl;       
+}
+
+
+void Armazem::esvaziaPrincipal(Secao secao)
+{
+    //enquanto a pilha principal da secao não estiver vazia
+    for(int j = 0; j < secao.principal.getTam(); j++)
+    {
+        //passa todos os pacotes da pilha principal para a auxiliar
+        //registrar o custo de retirada, que será a quantidade de pacotes movidos * custoRemocao
+        //o valor de custo remocao deve ser incluído no CLK do sistema
+        Pacote aux = secao.principal.Desempilha();
+        secao.auxiliar.Empilha(aux);
+        //talvez uma variável tempo para imprimir corretamente os tempos
+        //o transporte só pode ocorrer após essa remoção
+        std::cout << "pacote " << aux.getId() << " removido de " << this->idGrafo << " na secao " << secao.destino << std::endl;
+    }
+}
+
+void Armazem::carregaTransporte(int capacidade, int destino, Secao secao)
+{
+    while(capacidade > 0)
+    {
+        Pacote aux = secao.auxiliar.Desempilha();
+        capacidade--;
+        //nesse método também devemos atualizar o estado do pacote
+        std::cout << "pacote " << aux.getId() << " em transito de " << this->idGrafo << " para " << secao.destino << std::endl;
+    }
+}
+
+//passa os pacotes que não puderam ser transportados de volta para a fila principal
+void Armazem::retornaPrincipal(Secao secao)
+{
+    for(int i = 0; i < secao.auxiliar.getTam(); i++)
+    {
+        Pacote aux = secao.auxiliar.Desempilha();
+        secao.principal.Empilha(aux);
+        //também devemos alterar o estado do pacote nesse método
+        std::cout << "pacote " << aux.getId() << "rearmazenado em " << this->idGrafo << " na secao " << secao.destino << std::endl;
+    }
+}
